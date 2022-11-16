32d09bce7f218cb743f546a9c03ebfa0a09dd705
josewaihiga <josewaihiga@gmail.com>
2022-11-16 22:47:58 +1100

    more work on handling order submission

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -18,15 +18,27 @@ export default function OrderPage({ data }) {
     email: '',
   });
 
-  const { order, addToOrder, removeFromOrder } = usePizza({
+  const {
+    order,
+    addToOrder,
+    removeFromOrder,
+    error,
+    loading,
+    message,
+    submitOrder,
+  } = usePizza({
     pizzas,
-    inputs: values,
+    values,
   });
+
+  if (message) {
+    return <p>{message}</p>;
+  }
 
   return (
     <>
       <SEO title="Order a Pizza!" />
-      <OrderStyles>
+      <OrderStyles onSubmit={submitOrder}>
         <fieldset>
           <legend>Your Info</legend>
           <label htmlFor="name">Name</label>
@@ -39,7 +51,7 @@ export default function OrderPage({ data }) {
           />
           <label htmlFor="email">Email</label>
           <input
-            type="text"
+            type="email"
             name="email"
             id="email"
             value={values.email}
@@ -90,7 +102,12 @@ export default function OrderPage({ data }) {
           <h3>
             Your Total is {formatMoney(calculateOrderTotal(order, pizzas))}
           </h3>
-          <button type="submit">Order Ahead</button>
+
+          <div>{error ? <p>Error: {error}</p> : ''}</div>
+
+          <button type="submit" disabled={loading}>
+            {loading ? 'Placing Order...' : 'Order Ahead...'}
+          </button>
         </fieldset>
       </OrderStyles>
     </>
