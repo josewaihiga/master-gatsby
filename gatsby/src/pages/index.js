93426830e5fbfff51d3d38503567e3749415a9d4
josewaihiga <josewaihiga@gmail.com>
2023-03-18 18:09:06 +1100

    the > a

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -28,7 +28,7 @@ function HotSlices({ hotSlices }) {
       <h2 className="center">
         <span className="mark tilt">Hot slices</span>
       </h2>
-      <p>Come on by, buy the slice!</p>
+      <p>Come on by, buy a slice!</p>
       {!hotSlices && <LoadingGrid count={4} />}
       {hotSlices && !hotSlices?.length && <p>Nothin' in the case!</p>}
       {hotSlices?.length && <ItemGrid items={hotSlices} />}
